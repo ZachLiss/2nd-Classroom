2884d18caefb49c05ea75dcd9e3f0f5dadb133b5
PeterLancaster <peterlancast@gmail.com>
2013-12-01 15:55:00 -0500

    Create postnote.php

---
+++ postnote.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+$con = mysqli_connect("localhost", "admin", "admin", "2nd_classroom_db");
+if(mysqli_connect_errno($con)) {
+	echo "Failed to connect to MySQL: " . mysqli_connect_error();
+} else {
+
+	$poster = $_SESSION["username"];
+	$title = $_GET["title"];
+	$message = $_GET["note"];
+	$group_id = $_GET["group_id"];
+	//find if class exists
+	$content = mysqli_query($con, "INSERT INTO MESSAGES (poster, title, message, group_id ) VALUES ('$poster', '$title', '$message', '$group_id')");
+	}
+
+?>
